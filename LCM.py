ff8084d767aa7e7530f66b85e2b69d9ae2a05ab7
Bandi Nitesh <21P31A0409@acet.ac.in>
2024-01-01 23:17:59 +0530

    def gcd(a, b):     while b:         a, b = b, a%b     return a
    def lcm(a, b):
        return abs((a*b)//gcd(a, b))
        
    num1, num2 = map(int, input().split())
    print(lcm(num1, num2))

---
+++ LCM.py
@@ -0,0 +1,10 @@
+def gcd(a, b):
+    while b:
+        a, b = b, a%b
+    return a
+
+def lcm(a, b):
+    return abs((a*b)//gcd(a, b))
+    
+num1, num2 = map(int, input().split())
+print(lcm(num1, num2))
